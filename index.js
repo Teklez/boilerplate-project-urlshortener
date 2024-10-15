a9f038bad9673849fad3348872ea0bfb57ea0901
Teklez <zemenumekiria@gmail.com>
2024-10-15 09:21:14 +0300

    exercise finished

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const urlParser = require("url");
 const app = express();
 const dns = require("dns");
 const mongoose = require("mongoose");
@@ -12,12 +13,7 @@ const urlSchema = new mongoose.Schema({
   short_url: Number,
 });
 
-const lastShortUrl = new mongoose.Schema({
-  short_url: { type: Number, default: 0 },
-});
-
 const Url = mongoose.model("Url", urlSchema);
-const LastShortUrl = mongoose.model("LastShortUrl", lastShortUrl);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -38,27 +34,22 @@ app.get("/api/hello", function (req, res) {
 app.get("/api/shorturl/:shorturl", async (req, res) => {
   const shorturl = req.params.shorturl;
   if (!isNaN(shorturl)) {
-    const url = await Url.findOne({ short_url: Number(shorturl) });
+    const url = await Url.findOne({ short_url: shorturl });
     if (url) {
-      res.redirect("https://" + url.original_url);
+      res.redirect(url.original_url);
     }
   }
 });
 
 app.post("/api/shorturl", async (req, res) => {
-  let url = req.body.url;
+  const url = req.body.url;
   console.log("url", url);
   if (!url) {
     res.json({ error: "invalid URL" });
     return;
   }
-
-  // remove http:// or https://
-  url = url.replace(/(^\w+:|^)\/\//, "");
-  
-
-  dns.lookup(url, async (err, address, family) => {
-    if (err) {
+  dns.lookup(urlParser.parse(url).hostname, async (err, address, family) => {
+    if (err || !address) {
       res.json({ error: "invalid URL" });
     } else {
       const urlAlreadyExists = await Url.findOne({ original_url: url }).exec();
@@ -66,14 +57,14 @@ app.post("/api/shorturl", async (req, res) => {
       if (urlAlreadyExists) {
         res.json({ original_url: url, short_url: urlAlreadyExists.short_url });
       } else {
-        const lastShortUrl = await LastShortUrl.findOne().exec();
-        const newShortUrl = lastShortUrl.short_url + 1;
-        const newUrl = new Url({ original_url: url, short_url: newShortUrl });
-        const newLastShortUrl = new LastShortUrl({ short_url: newShortUrl });
-        await newLastShortUrl.save();
+        const urlCount = await Url.countDocuments();
+        const newUrl = new Url({
+          original_url: url,
+          short_url: urlCount + 1,
+        });
         await newUrl.save();
 
-        res.json({ original_url: url, short_url: newShortUrl });
+        res.json({ original_url: url, short_url: urlCount + 1 });
       }
     }
   });
